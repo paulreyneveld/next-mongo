33df7565ab456a7ff63ade4808bc33fe7753588e
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-05-27 16:14:03 -0700

    Fixed error with regard to name conflict and removed extraneous console

--- pages/api/movies/[id].js
+++ pages/api/movies/[id].js
@@ -3,8 +3,6 @@ import { connectToDatabase } from "../../../util/mongodb"
 export default async (req, res) => {
     const { db } = await connectToDatabase();
     const { id } = req.query;
-    const movie = null;
-    console.log(id);
 
     const movie = await db
         .collection("movies")
