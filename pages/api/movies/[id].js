f4595cfb5b8d91df4b401b41f4be84d5b63c2841
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-05-27 15:52:17 -0700

    Revert "Used dynamic routing and an api end point to return a single movie"
    This reverts commit 35588b91913967e756e1f7bf7263648ecf56ddcb.
    This restores the api endpoint method for data retrieval from the db

--- pages/api/movies/[id].js
+++ pages/api/movies/[id].js
@@ -1,16 +1,14 @@
 import { connectToDatabase } from "../../../util/mongodb"
-import { ObjectId } from 'mongodb'
 
 export default async (req, res) => {
     const { db } = await connectToDatabase();
     const { id } = req.query;
-    console.log(typeof id);
+    const movie = null;
     console.log(id);
 
     const movie = await db
         .collection("movies")
-        .find({ _id: ObjectId(id) })
-        .toArray();
+        .findById(id)
  
     res.json(movie);
 }
