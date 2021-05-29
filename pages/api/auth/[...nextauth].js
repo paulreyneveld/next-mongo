07971c70b47718cf245f8a55a559b60e5acc5e86
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-05-29 14:55:11 -0700

    Updated api/auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,25 @@
-// pages/api/auth/[...nextauth].js
-import NextAuth from 'next-auth'
+// pages/api/auth/[…nextauth].js
+// ...other imports
+import Providers from 'next-auth/providers'
 
 const options = {
-  site: process.env.NEXTAUTH_URL
+  site: process.env.NEXTAUTH_URL,
+  providers: [
+    Providers.Email({
+      server: {
+        port: 465,
+        host: 'smtp.gmail.com',
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    })
+  ],
+  database: process.env.DATABASE_URL
 }
-
-export default (req, res) => NextAuth(req, res, options)
