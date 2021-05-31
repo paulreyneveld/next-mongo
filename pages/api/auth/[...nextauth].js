ff60915fd35a5126d11398f96b6ef404e3d2f1d1
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-05-30 17:18:24 -0700

    Trying to sort out auth issue

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,25 +1,8 @@
-// pages/api/auth/[…nextauth].js
-// ...other imports
-import Providers from 'next-auth/providers'
+// pages/api/auth/[...nextauth].js
+import NextAuth from 'next-auth'
 
 const options = {
-  site: process.env.NEXTAUTH_URL,
-  providers: [
-    Providers.Email({
-      server: {
-        port: 465,
-        host: 'smtp.gmail.com',
-        secure: true,
-        auth: {
-          user: process.env.EMAIL_USERNAME,
-          pass: process.env.EMAIL_PASSWORD,
-        },
-        tls: {
-          rejectUnauthorized: false,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-    })
-  ],
-  database: process.env.DATABASE_URL
+  site: process.env.NEXTAUTH_URL
 }
+
+export default (req, res) => NextAuth(req, res, options)
