b96ce1010cd5fdf4fe6316a0451f48cc2cab81a9
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-05-30 17:21:00 -0700

    Checking to see if spread operator in file name caused glitch

---
+++ pages/api/auth/[. . .nextauth].js
@@ -0,0 +1,26 @@
+// pages/api/auth/[...nextauth].js
+import NextAuth from 'next-auth'
+
+const options = {
+  site: process.env.NEXTAUTH_URL,
+  providers: [
+    Providers.Email({
+      server: {
+        port: 465,
+        host: 'smtp.gmail.com',
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    })
+  ],
+  database: process.env.DATABASE_URL
+}
+
+export default (req, res) => NextAuth(req, res, options)
--- pages/api/auth/[...nextauth].js
+++
@@ -1,8 +0,0 @@
-// pages/api/auth/[...nextauth].js
-import NextAuth from 'next-auth'
-
-const options = {
-  site: process.env.NEXTAUTH_URL
-}
-
-export default (req, res) => NextAuth(req, res, options)
